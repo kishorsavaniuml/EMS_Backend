4c36a63589ed70c55d84ff9442c1a1496b750953
kishorsavaniuml <kishorsavaniuml@gmail.com>
2026-06-15 17:19:31 +0530

    frontend cors origin fixed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://ems-frontend-40r4hlxsq-kishorsavaniuml-6951s-projects.vercel.app",
+    origin:"https://ems-frontend-raecnr8xx-kishorsavaniuml-6951s-projects.vercel.app",
     credentials:true
 }))
 ""
